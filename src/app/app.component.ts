1b8c7d2b725c745954efc41c53d175fc0e95ffc2
kokosanki <os_mo@wp.pl>
2019-04-19 18:25:25 +0200

    todo list angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+  export class AppComponent {
+    title = 'My To Do List APP';
+}
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss'],
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() item: TodoItem;
+  
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+  }
+
+  completeItem() {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
+  }
+  
+  removeItem() {
+    this.remove.emit(this.item);
+  }
+
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
+  
+}
+
